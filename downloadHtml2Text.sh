c7d448426c5ccf983da08c5c3a14064a6a75ad5b
teicheld <teicheld@web.de>
2023-10-12 11:38:18 +0000

    Update downloadHtml2Text.sh
    better file name generation/extraction

--- downloadHtml2Text.sh
+++ downloadHtml2Text.sh
@@ -1,5 +1,6 @@
 [ -z $1 ] && echo provide url and optional the destinationDir to save to && exit
 name="$(echo $1 | rev | cut -d '/' -f1 | rev)"
+if [ -z "$name" ]; then name="$(echo $1 | rev | cut -d '/' -f2 | rev)"; fi
 [ -n "$2" ] && destinationDir=$2 || destinationDir=~/media/text/
 destination="${destinationDir}/${name}.txt"
 curl "$1" | html2text > "$destination"
